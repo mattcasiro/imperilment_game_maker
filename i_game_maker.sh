4816c44f269a66479490c39cde553c1e8912e55e
Matthew Casiro <mattcasiro@gmail.com>
2016-08-29 19:12:20 -0700

    minor bug fixes

--- i_game_maker.sh
+++ i_game_maker.sh
@@ -1,5 +1,5 @@
 if [ $# == 4 ]; then
-  ruby -I ./lib -I . run.rb $1 $2 $3 $4
+  ruby -I ./lib -I . submit_game.rb $1 $2 $3 $4
 else
   echo "Usage: bash i_game_maker.sh <num games> <start date (YYYY-MM-DD)> <user> <pwd>"
 fi
