0b9d971ef587d914c151954581fccf4ec404b520
Matthew Casiro <mattcasiro@gmail.com>
2016-09-09 15:15:58 -0700

    Add support for scheduling jobs with Whenever

---
+++ config/schedule.rb
@@ -0,0 +1,28 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+
+# set :output, "~/Documents/Stembolt/imperilment_game_maker/cron.log"
+# 
+# every :monday, at: '8am' do
+#   command "echo \"Creating new imperilment on: $(date)\""
+#   command "ruby ~/Documents/Stembolt/imperilment_game_maker/submit_game.rb http://localhost:3000 admin@example.com test123"
+# end
