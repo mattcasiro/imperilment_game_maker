0558a6cc079f0d0559f577f40628c97776e6ec0b
Matthew Casiro <mattcasiro@gmail.com>
2016-09-19 15:15:39 -0700

    Move fixtures dir to spec dir

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,3 +1,5 @@
-FIXTURE_PATH = 'spec/game_builder/fixtures/'
+FIXTURE_PATH = 'spec/fixtures/'
+WEB_PATH = 'imperilment.stembolt.com'
 LINK_STR = "a href="
 VDD_STR = "[Video Daily Double]"
+TOKEN_REGEX = "//meta[@name='csrf-token']"
