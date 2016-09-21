26ac7a1e755920fd158c98924454deaac1e34d51
Matthew Casiro <mattcasiro@gmail.com>
2016-09-20 17:08:21 -0700

    Add support for settings in constants, and move clue cleaning out

--- lib/game_submitter/clue_selector.rb
+++ lib/game_submitter/clue_selector.rb
@@ -1,4 +1,5 @@
 require 'game_submitter/cleaner'
+require 'constants'
 
 module GameSubmitter
   class ClueSelector
@@ -6,32 +7,30 @@ module GameSubmitter
     #
     # @param clue_array [Array] an array of clues to select from
     def initialize clue_array
-      @clues = clean_clues clue_array
-      @level = level
+      @clues = clue_array
     end
 
     # Get a set of clues from the provided array
     #
     # @param level [Sym] determines which clues are selected
     # @returns [Array] an array of selected clues
-    def select_clues level=:easy
-      case level
-      when :easy
-        clues[0..2]
-      when :medium, :hard
-        clues[(clues.size - 3)..-1]
+    def select_clues
+      if clues.size.eql?(1)
+        # Handle Final Jeopardy exception
+        [clues[0]]
       else
-        raise ArgumentError, "Invalid difficulty, use :easy, :medium, or :hard"
+        case DIFFICULTY
+        when :easy
+          clues[0..2]
+        when :medium, :hard
+          clues[(clues.size - 3)..-1]
+        else
+          raise ArgumentError, "Invalid difficulty in constants.rb"
+        end
       end
     end
 
     private
-    attr_accessor :clues, :level
-
-    def clean_clues clues
-      clues.map do |clue|
-        Cleaner.new.clean_clue clue
-      end
-    end
+    attr_accessor :clues
   end
 end
--- spec/game_submitter/clue_selector_spec.rb
+++ spec/game_submitter/clue_selector_spec.rb
@@ -4,23 +4,34 @@ require 'constants'
 
 module GameSubmitter
   describe ClueSelector do
-    context "Given five clues" do
+    context "Given five clues on easy difficulty" do
       let(:clues) { GameBuilder::Scraper.new.new_game!(FIXTURE_PATH + "show_465_id_4788.html").rounds[0].categories[3].clues }
       let(:selector) { ClueSelector.new(clues) } 
 
       it { expect(selector.select_clues).to be_an_instance_of(Array) }
       it { expect(selector.select_clues.size).to be(3) }
-      it { expect(selector.select_clues).to eql(clues[0..2]) }
+      it "should select the first three clues" do
+        stub_const("DIFFICULTY", :easy)
+        expect(selector.select_clues).to eql(clues[0..2])
+      end
     end
 
     context "Given different difficulties" do
       let(:clues) { GameBuilder::Scraper.new.new_game!(FIXTURE_PATH + "show_465_id_4788.html").rounds[0].categories[3].clues }
       let(:selector) { ClueSelector.new(clues) } 
 
-      it { expect(selector.select_clues(:easy)).to eql(clues[0..2]) }
-      it { expect(selector.select_clues(:medium)).to eql(clues[2..4]) }
-      it { expect(selector.select_clues(:hard)).to eql(clues[2..4]) }
-      it { expect{ selector.select_clues(:ez) }.to raise_error(ArgumentError) }
+      it "should select last three clues on medium" do
+        stub_const("DIFFICULTY", :medium)
+        expect(selector.select_clues).to eql(clues[2..4])
+      end
+      it "should select last three clues on hard" do
+        stub_const("DIFFICULTY", :hard)
+        expect(selector.select_clues).to eql(clues[2..4])
+      end
+      it "should raise and error give an invalid difficulty" do
+        stub_const("DIFFICULTY", :ez)
+        expect{ selector.select_clues }.to raise_error(ArgumentError)
+      end
     end
   end
 end
