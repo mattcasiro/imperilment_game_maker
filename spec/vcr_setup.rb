8b9c0e0d989b10242e2dc882cf3c834ece3fe997
Matthew Casiro <mattcasiro@gmail.com>
2016-09-20 17:15:13 -0700

    Remove VCR logging

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -5,5 +5,5 @@ VCR.configure do |c|
   c.cassette_library_dir = FIXTURE_PATH + 'vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.debug_logger = File.open('vcr.log', 'w')
+  # c.debug_logger = File.open('vcr.log', 'w')
 end
